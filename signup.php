c12ab1403cd8eb7fbc330839a2f22316751b9d35
AudreyArmontrout <armontae@miamioh.edu>
2023-04-07 15:57:51 -0400

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,6 +1,6 @@
 <?php
     $conn = new mysqli("sql9.freesqldatabase.com", "sql9610600", "ZE1RGDLzSD");
-    // Check connection
+    // Check connections
     if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
     }
