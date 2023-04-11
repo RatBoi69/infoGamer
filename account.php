df2dfa473b43cc4335058af6754b145c3afd0272
AudreyArmontrout <armontae@miamioh.edu>
2023-04-11 15:16:38 -0400

    Update account.php

--- account.php
+++ account.php
@@ -1,20 +1,28 @@
 <html>
 <body>
     <?php
-    $conn = new mysqli("sql9.freesqldatabase.com", "sql9610600", "ZE1RGDLzSD");
-    // Check connection
-    if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-    }
+    ini_set('display_errors', 1); 
+    ini_set('display_startup_errors', 1); 
+    error_reporting(E_ALL);
+        $servername = "sql9.freesqldatabase.com";
+        $username = "sql9610600";
+        $password = "ZE1RGDLzSD";
+        $dbname = "sql9610600";
+        
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        
+        // Check connection
+        if ($conn->connect_error) {
+          die("Connection failed: " . $conn->connect_error);
+        }
 
     $cookie_name = "ArcadeLegacyUID";
     if(!isset($_COOKIE[$cookie_name])) {
         header('Location: login.html');
         $conn->close();
-        exit;
     } else {
-
-        $_COOKIE[$cookie_name];
+        echo "Welcome" . $_COOKIE[$cookie_name];
     }
     ?>  
 </body>
