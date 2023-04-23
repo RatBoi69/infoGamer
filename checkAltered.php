3d3ea2b6930ea4b27732970957a68ad4a6b44194
AudreyArmontrout <armontae@miamioh.edu>
2023-04-23 05:58:33 -0400

    Update checkAltered.php

--- checkAltered.php
+++ checkAltered.php
@@ -28,14 +28,13 @@ if(!isset($_COOKIE[$cookie_name])) {
     // Loop to store and display values of individual checked checkbox.
         $game = $_POST['check_list'];
     // clear favorites table where users = cookie
-    $sql = "DELETE FROM favorites WHERE User_ID=$identify";
+    $sql = "DELETE FROM favorites WHERE User_ID='$identify'";
 
     foreach($game as $check_list){
         // pull from favorites where users = cookie and game id = this
         // if null add it
-        $sql = "INSERT INTO favorites (Favorite_ID, User_ID, Game_ID)
-        VALUES (NULL, $identify, $check_list)";
-
+        $sql = "INSERT INTO favorites (User_ID, Game_ID) VALUES ('$identify', '$check_list')";
     }
     }
+    header('Location: indexlogin.php');
 ?>
