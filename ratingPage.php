8bdcd9dcc03ca3d5a7e4d176e6b2e9fbcaa68786
AudreyArmontrout <armontae@miamioh.edu>
2023-04-26 11:36:11 -0400

    Update ratingPage.php

--- ratingPage.php
+++ ratingPage.php
@@ -31,90 +31,87 @@
     <style>
         /* Style the dropdown button */
         .dropdown {
-  position: relative;
-  display: inline-block;
-}
+            position: relative;
+            display: inline-block;
+        }
 
-/* Style the dropdown content */
-.dropdown-content {
-  display: none;
-  position: absolute;
-  z-index: 1;
-  background-color: #f1f1f1;
-  margin-top: 5px;
-  margin-left: -120;
-}
+        /* Style the dropdown content */
+        .dropdown-content {
+            display: none;
+            position: absolute;
+            z-index: 1;
+            background-color: #f1f1f1;
+            margin-top: 5px;
+            margin-left: -120;
+        }
 
-/* Style the links in the dropdown menu */
-.dropdown-content a {
-  display: block;
-  padding: 12px 16px;
-  text-decoration: none;
-  color: white;
-}
+        /* Style the links in the dropdown menu */
+        .dropdown-content a {
+            display: block;
+            padding: 12px 16px;
+            text-decoration: none;
+            color: white;
+        }
 
-/* Style the links in the dropdown menu when hovering over them */
-.dropdown-content a:hover {
-  background-color: #00DBFF;
-}
+        /* Style the links in the dropdown menu when hovering over them */
+        .dropdown-content a:hover {
+            background-color: #00DBFF;
+        }
     </style>
 
     <script>
-function myFunction() {
-var x = document.getElementById("myTopnav");
-if (x.className === "topnav") {
-x.className += " responsive";
-} else {
-x.className = "topnav";
-}
-}
-</script>
-
+        function myFunction() {
+            var x = document.getElementById("myTopnav");
+            if (x.className === "topnav") {
+                x.className += " responsive";
+            } else {
+                x.className = "topnav";
+            }
+        }
+    </script>
 
 </head>
 <body>
-
     <header>
 		<nav class="topnav" id="myTopnav" >
             <div style="display: flex; justify-content: space-between;">
-                
-                    <img src="img/ALlogo.png" width = "200px" style="padding: 5px 20px;" alt="Logo"/>
+                <img src="img/ALlogo.png" width = "200px" style="padding: 5px 20px;" alt="Logo"/>
                 <div class="links" style="padding: 20px 0px 0px 380px;" float="right">
                     <a href="indexlogin.php">Home</a>
                     <a href="about_us_page_logged_In.html">About Us</a>
                     <a href="costLogin.html">Cost</a>
                 </div>
 
-		<!-- Create the clickable image -->
-  		<div class="dropdown">
-    		<img src="circle-user-solid.svg" width = "50px" style="padding: 15px 20px; position: relative; top: 0px; left: -120px;" alt="User Image" onclick="toggleDropdown()">
-    		<!-- Create the dropdown menu -->
-    		<div class="dropdown-content" id="dropdown">
-      	<div>
-        	<a href="account.php"><b>Profile</b></a>
-      	</div>
-      	<div>
-        		<a href="https://www.website2.com"><b>Favorites</b></a>
-      	</div>
-		    <div>
-		      	<a href="index.php"><b>Log Out</b></a>
-		    </div>
+                <!-- Create the clickable image -->
+                <div class="dropdown">
+                    <img src="circle-user-solid.svg" width = "50px" style="padding: 15px 20px; position: relative; top: 0px; left: -120px;" alt="User Image" onclick="toggleDropdown()">
+                    <!-- Create the dropdown menu -->
+                    <div class="dropdown-content" id="dropdown">
+                    <div>
+                        <a href="account.php"><b>Profile</b></a>
+                    </div>
+                    <div>
+                        <a href="https://www.website2.com"><b>Favorites</b></a>
+                    </div>
+                    <div>
+                        <a href="index.php"><b>Log Out</b></a>
+                    </div>
 
-		  <script>
-			function toggleDropdown() {
-			  var dropdown = document.getElementById("dropdown");
-			  if (dropdown.style.display === "block") {
-				dropdown.style.display = "none";
-			  } else {
-				dropdown.style.display = "block";
-			  }
-			}
-		  </script>
-    </div>
-  </div>
+                    <script>
+                        function toggleDropdown() {
+                        var dropdown = document.getElementById("dropdown");
+                        if (dropdown.style.display === "block") {
+                            dropdown.style.display = "none";
+                        } else {
+                            dropdown.style.display = "block";
+                        }
+                        }
+                    </script>
+                </div>
+            </div>
 		</nav>
-			
-		</header>
+	</header>
 
-        </body>
-        </html>
+    
+</body>
+</html>
