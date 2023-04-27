e8b09970836fede634c3ceb6d5717c49b1455987
AudreyArmontrout <armontae@miamioh.edu>
2023-04-27 00:26:30 -0400

    rearranging unused files

--- indexlogin.php
+++ indexlogin.php
@@ -1,35 +1,33 @@
 <?php
-    ini_set('display_errors', 1); 
-    ini_set('display_startup_errors', 1); 
-    error_reporting(E_ALL);
-        $servername = "sql.freedb.tech";
-        $username = "freedb_cse201";
-        $password = "?NHU8n?7FWyYUPK";
-        $dbname = "freedb_cse201";
-        
-        // Create connection
-        $conn = new mysqli($servername, $username, $password, $dbname);
-        
-        // Check connection
-        if ($conn->connect_error) {
-          die("Connection failed: " . $conn->connect_error);
-        }
-        
-        $cookie_name = "ArcadeLegacyUID";
-        if(!isset($_COOKIE[$cookie_name])) {
-          header('Location: login.php');
-          $conn->close();
-        } else {
-          $identify = $_COOKIE[$cookie_name];
-        }
+  ini_set('display_errors', 1); 
+  ini_set('display_startup_errors', 1); 
+  error_reporting(E_ALL);
+  $servername = "sql.freedb.tech";
+  $username = "freedb_cse201";
+  $password = "?NHU8n?7FWyYUPK";
+  $dbname = "freedb_cse201";
+  
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  
+  $cookie_name = "ArcadeLegacyUID";
+  if(!isset($_COOKIE[$cookie_name])) {
+    header('Location: login.php');
+    $conn->close();
+  } else {
+    $identify = $_COOKIE[$cookie_name];
+  }
 
-        $sql = "SELECT * from games";
-        $result = $conn->query($sql);
+  $sql = "SELECT * from games";
+  $result = $conn->query($sql);
 
-        $sqlFav = "SELECT * from favorites WHERE User_ID=$identify";
-        $favs = $conn->query($sqlFav);
-
-
+  $sqlFav = "SELECT * from favorites WHERE User_ID=$identify";
+  $favs = $conn->query($sqlFav);
 ?>  
 
 <html>
